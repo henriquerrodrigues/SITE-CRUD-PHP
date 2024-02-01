645fd963e23b8ec687cf386eacb88971fc2a4ac9
Henrique Ribeiro Rodrigues <henriquerrodrigues3@gmail.com>
2024-02-01 16:17:26 -0300

    commit

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -4,13 +4,17 @@
     {
         public function index()
         {
-             $loader = new \Twig\Loader\FilesystemLoader('app/View');
-             $twig = new \Twig\Environment($loader);
-             $template = $twig->load('admin.html');
- 
-             $parametros = array();
-             $conteudo = $template->render($parametros);
-             echo $conteudo;
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            $template = $twig->load('admin.html');
+
+            $objPostagens = Postagem::selecionaTodos();
+
+            $parametros = array();
+            $parametros['postagens'] = $objPostagens;    
+
+            $conteudo = $template->render($parametros);
+            echo $conteudo;
         } 
 
         public function create()
@@ -34,6 +38,51 @@
             catch(Exception $e){
                 echo '<script>alert("'.$e->getMessage().'");</script>';
                 echo '<script>location.href="http://localhost/SITE-CRUD-PHP/?pagina=admin&metodo=create"</script>';
+            }
+        }
+
+        public function change($paramId)
+        {
+            $loader = new \Twig\Loader\FilesystemLoader('app/View');
+            $twig = new \Twig\Environment($loader);
+            $template = $twig->load('update.html');
+
+            $post = Postagem::selecionaPorId($paramId);
+
+            $parametros = array();
+            $parametros['id'] = $post->id;
+            $parametros['titulo'] = $post->titulo;
+            $parametros['conteudo'] = $post->conteudo;
+
+            $conteudo = $template->render($parametros);
+            echo $conteudo;
+        }
+
+        public function update()
+        {
+            try{
+                Postagem::update($_POST);
+
+                echo '<script>alert("Publicação alterada com sucesso!");</script>';
+                echo '<script>location.href="http://localhost/SITE-CRUD-PHP/?pagina=admin&metodo=index"</script>';                
+            }
+            catch(Exception $e){
+                echo '<script>alert("'.$e->getMessage().'");</script>';
+                echo '<script>location.href="http://localhost/SITE-CRUD-PHP/?pagina=admin&metodo=change&id='.$_POST['id'].'"</script>'; 
+            }  
+             
+        }
+
+        public function delete($paramId)
+        {
+            try{
+                Postagem::delete($paramId);
+                echo '<script>alert("Publicação deletada com sucesso!");</script>';
+                echo '<script>location.href="http://localhost/SITE-CRUD-PHP/?pagina=admin&metodo=index"</script>';                
+            }
+            catch(Exception $e){
+                echo '<script>alert("'.$e->getMessage().'");</script>';
+                echo '<script>location.href="http://localhost/SITE-CRUD-PHP/?pagina=admin&metodo=index"</script>'; 
             }
         }
      }
--- app/Model/Postagem.php
+++ app/Model/Postagem.php
@@ -25,26 +25,25 @@ require_once 'lib/Database/Connection.php';
             return $resultado;
         }
 
-        public static function selecionarPorId($idPost)
-        {
-            $con = Connection::getConn();
+		public static function selecionaPorId($idPost)
+		{
+			$con = Connection::getConn();
 
-            $sql = "SELECT * FROM postagem WHERE id = :id";
-            $sql = $con->prepare($sql);
-            $sql->bindValue(':id', $idPost, PDO::PARAM_INT);
-            $sql->execute();
+			$sql = "SELECT * FROM postagem WHERE id = :id";
+			$sql = $con->prepare($sql);
+			$sql->bindValue(':id', $idPost, PDO::PARAM_INT);
+			$sql->execute();
 
-            $resultado = $sql->fetchObject('Postagem');
+			$resultado = $sql->fetchObject('Postagem');
 
-            if(!$resultado){
-                throw new Exception("Não foi encontrado nenhum registro no banco");
-            }
-            else{
-                $resultado->comentarios = Comentario::selecionarComentarios($resultado->id);
-            }
-            
-            return $resultado;
-        }
+			if (!$resultado) {
+				throw new Exception("Não foi encontrado nenhum registro no banco");	
+			} else {
+				$resultado->comentarios = Comentario::selecionarComentarios($resultado->id);
+			}
+
+			return $resultado;
+		}
         
         public static function insert($dadosPost)
         {
@@ -70,5 +69,43 @@ require_once 'lib/Database/Connection.php';
                return false;
             }
                 return true;
+        }
+
+        public static function update($params)
+        {
+            $con = Connection::getConn();
+
+            $sql = "UPDATE postagem SET titulo = :tit, conteudo = :cont WHERE id = :id";
+            $sql = $con->prepare($sql);
+            $sql->bindValue(':tit', $params['titulo']);
+            $sql->bindValue(':cont', $params['conteudo']);
+            $sql->bindValue(':id', $params['id']);
+            $resultado = $sql->execute();
+
+            if($resultado == 0){
+                throw new Exception("Falha ao alterar publicação");
+
+                return false;
+            }
+
+            return true;
+
+        }
+
+        public static function delete($id)
+        {
+            $con = Connection::getConn();
+
+            $sql = "DELETE FROM postagem  WHERE id = :id";
+            $sql = $con->prepare($sql);
+            $sql->bindValue(':id', $id);
+            $resultado = $sql->execute();
+
+            if($resultado == 0){
+                throw new Exception("Falha ao deletar publicação");
+                return false;
+            }
+
+            return true;
         }
     }
